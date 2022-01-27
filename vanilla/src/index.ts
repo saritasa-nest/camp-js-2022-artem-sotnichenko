de5bc953fb7ffd868867637dcc36e6590aec78c4
Artem Sotnichenko <artem.sotnichenko@saritasa.com>
2022-01-27 16:39:42 +0700

    Add sort functionality #JC19-255

--- vanilla/src/index.ts
+++ vanilla/src/index.ts
@@ -4,6 +4,148 @@ import { Film } from './firebase/film/types';
 
 const FILMS_COUNT_PER_PAGE = 2;
 
+let lastId: string | undefined;
+let firstId: string | undefined;
+
+let isLastPage = false;
+let isFirstPage = false;
+
+const sortField = 'title';
+const sortType = 'ascending';
+
+document.addEventListener('DOMContentLoaded', async e => {
+  // Disable buttons before films are fetched.
+  isFirstPage = true;
+  isLastPage = true;
+  updatePaginationButtons();
+  isFirstPage = false;
+  isLastPage = false;
+
+  await loadNextPage();
+
+  // Disable "prev" button.
+  isFirstPage = true;
+  updatePaginationButtons();
+
+  const {
+    paginationNextEl,
+    paginationPrevEl,
+  } = getPaginationElements();
+
+  paginationNextEl?.addEventListener('click', loadNextPage);
+  paginationPrevEl?.addEventListener('click', loadPrevPage);
+
+  const sortFieldEl = document.querySelector<HTMLSelectElement>('.films__sort--field .sort__select');
+  const sortTypeEl = document.querySelector<HTMLSelectElement>('.films__sort--type .sort__select');
+
+  if (!sortFieldEl || !sortTypeEl) {
+    return;
+  }
+
+  sortFieldEl.addEventListener('change', () => {
+    loadNextPage();
+
+  });
+});
+
+interface PaginationElements {
+
+  /** Next page button. */
+  paginationNextEl: HTMLButtonElement | null;
+
+  /** Previous page button. */
+  paginationPrevEl: HTMLButtonElement | null;
+}
+
+/** Get pagination elements. */
+function getPaginationElements(): PaginationElements {
+  return {
+    paginationNextEl: document.querySelector<HTMLButtonElement>('.pagination__next'),
+    paginationPrevEl: document.querySelector<HTMLButtonElement>('.pagination__prev'),
+  };
+}
+
+/** Load next page of films. */
+async function loadNextPage(): Promise<void> {
+  if (isLastPage) {
+    updatePaginationButtons();
+    return;
+  }
+
+  const films = await getFilmsAfterId({
+    limit: FILMS_COUNT_PER_PAGE,
+    orderBy: `fields.${sortField}`,
+    startAfter: lastId,
+  });
+
+  if (films.length < FILMS_COUNT_PER_PAGE) {
+    isLastPage = true;
+    return updatePaginationButtons();
+  }
+  isFirstPage = false;
+
+  lastId = films[films.length - 1].id;
+  firstId = films[0].id;
+
+  displayFilms(films);
+  updatePaginationButtons();
+}
+
+/** Load previous page of films. */
+async function loadPrevPage(): Promise<void> {
+  if (isFirstPage) {
+    updatePaginationButtons();
+    return;
+  }
+
+  const films = await getFilmsBeforeId({
+    limit: FILMS_COUNT_PER_PAGE,
+    orderBy: 'fields.title',
+    endBefore: firstId,
+  });
+
+  if (films.length < FILMS_COUNT_PER_PAGE) {
+    isFirstPage = true;
+    return updatePaginationButtons();
+  }
+  isLastPage = false;
+
+  lastId = films[films.length - 1].id;
+  firstId = films[0].id;
+
+  await displayFilms(films);
+  await updatePaginationButtons();
+}
+
+/** Update pagination buttons. */
+function updatePaginationButtons(): void {
+  const {
+    paginationNextEl,
+    paginationPrevEl,
+  } = getPaginationElements();
+
+  if (!paginationPrevEl || !paginationNextEl) {
+    return;
+  }
+
+  paginationNextEl.disabled = !!isLastPage;
+  paginationPrevEl.disabled = !!isFirstPage;
+}
+
+function getSortOptions() {
+  const sortFieldEl = document.querySelector<HTMLSelectElement>('.films__sort--field .sort__select');
+  const sortTypeEl = document.querySelector<HTMLSelectElement>('.films__sort--type .sort__select');
+
+  if (!sortFieldEl || !sortTypeEl) {
+    return;
+  }
+
+  return {
+    field: sortFieldEl.value,
+    type: sortTypeEl.value,
+  };
+}
+
 /**
  * Create film element.
  * @param film Film data.
@@ -35,91 +177,3 @@ function displayFilms(films: Film[]): void {
     filmsListEl?.appendChild(createFilmEl(film));
   });
 }
-
-let lastId: string | undefined;
-let firstId: string | undefined;
-
-let isLastPage = false;
-let isFirstPage = true;
-
-/** Load next page of films. */
-async function loadNextPage(): Promise<void> {
-  if (isLastPage) {
-    updatePaginationButtons();
-    return;
-  }
-
-  const films = await getFilmsAfterId({
-    limit: FILMS_COUNT_PER_PAGE,
-    orderBy: 'fields.title',
-    startAfter: lastId,
-  });
-
-  if (films.length < FILMS_COUNT_PER_PAGE) {
-    isLastPage = true;
-    return;
-  }
-  isFirstPage = false;
-
-  lastId = films[films.length - 1].id;
-  firstId = films[0].id;
-
-  await displayFilms(films);
-  updatePaginationButtons();
-}
-
-/** Load previous page of films. */
-async function loadPrevPage(): Promise<void> {
-  if (isFirstPage) {
-    updatePaginationButtons();
-    return;
-  }
-
-  const films = await getFilmsBeforeId({
-    limit: FILMS_COUNT_PER_PAGE,
-    orderBy: 'fields.title',
-    endBefore: firstId,
-  });
-
-  if (films.length < FILMS_COUNT_PER_PAGE) {
-    isFirstPage = true;
-    return;
-  }
-  isLastPage = false;
-
-  lastId = films[films.length - 1].id;
-  firstId = films[0].id;
-
-  await displayFilms(films);
-
-}
-
-/** Update pagination buttons. */
-function updatePaginationButtons(): void {
-  const filmsPaginationNextEl = document.querySelector<HTMLButtonElement>('.pagination__next');
-  const filmsPaginationPrevEl = document.querySelector<HTMLButtonElement>('.pagination__prev');
-
-  if (!filmsPaginationPrevEl || !filmsPaginationNextEl) {
-    return;
-  }
-
-  filmsPaginationNextEl.disabled = false;
-  filmsPaginationPrevEl.disabled = false;
-
-  if (isFirstPage) {
-    filmsPaginationPrevEl.disabled = true;
-  }
-  if (isLastPage) {
-    filmsPaginationNextEl.disabled = true;
-  }
-}
-
-await loadNextPage();
-isFirstPage = true;
-updatePaginationButtons();
-
-const filmsPaginationNextEl = document.querySelector<HTMLButtonElement>('.pagination__next');
-const filmsPaginationPrevEl = document.querySelector<HTMLButtonElement>('.pagination__prev');
-
-filmsPaginationNextEl?.addEventListener('click', loadNextPage);
-filmsPaginationPrevEl?.addEventListener('click', loadPrevPage);
