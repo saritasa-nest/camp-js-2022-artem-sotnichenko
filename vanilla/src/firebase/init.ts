d299dae0e9b8b4709a0f1c19de269ef17f4b5402
Artem Sotnichenko <artem.sotnichenko@saritasa.com>
2022-01-27 14:07:21 +0700

    Make env compatible #JC19-255

--- vanilla/src/firebase/init.ts
+++ vanilla/src/firebase/init.ts
@@ -2,9 +2,9 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 export const app = initializeApp({
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
 });
 
 export const db = getFirestore(app);
--- vanilla/vite.config.js
+++ vanilla/vite.config.js
@@ -1,5 +1,6 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import env from 'vite-plugin-env-compatible'
 
 const root = resolve(__dirname, 'src');
 const outDir = resolve(__dirname, 'dist');
@@ -7,7 +8,11 @@ const outDir = resolve(__dirname, 'dist');
 // https://vitejs.dev/config/
 export default defineConfig({
   root,
-  plugins: [],
+  plugins: [
+    env({
+      prefix: ''
+    }),
+  ],
   build: {
     outDir,
     emptyOutDir: true,
