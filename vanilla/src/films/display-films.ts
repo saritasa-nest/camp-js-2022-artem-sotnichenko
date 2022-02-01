bf3c7fe13ee534e222b441ddb4f24029e1967522
Artem Sotnichenko <artem.sotnichenko@saritasa.com>
2022-02-01 14:25:26 +0700

    Update styles #JC19-255

--- vanilla/src/films/display-films.ts
+++ vanilla/src/films/display-films.ts
@@ -1,6 +1,14 @@
 import { Film } from '../entities/film/types';
 
 /**
+ * Format date to "Month Day, Year" format.
+ * @param date Date object to format.
+ */
+function formatDate(date: Date): string {
+  return date.toLocaleDateString('en-us', { year: 'numeric', month: 'short', day: 'numeric' });
+}
+
+/**
  * Create film element.
  * @param film Film data.
  */
@@ -8,9 +16,16 @@ function createFilmEl(film: Film): HTMLDivElement {
   const node = document.createElement('div');
   node.classList.add('film');
   node.innerHTML = `
-  <div class="film__title">${film.title}</div>
-  <div class="film__producer">${film.producer}</div>
-  <div class="film__releaseDate">${film.releaseDate}</div>
+<div class="film__title">${film.title}</div>
+<div class="film__producer film__meta-info meta-info">
+  <div class="meta-info__label">produced by</div><div class="meta-info__text">${film.producer}</div>
+</div>
+<div class="film__director film__meta-info meta-info">
+  <div class="meta-info__label">directed by</div><div class="meta-info__text">${film.director}</div>
+</div>
+<div class="film__releaseDate film__meta-info meta-info">
+  <div class="meta-info__label">released at</div><div class="meta-info__text">${formatDate(film.releaseDate)}</div>
+</div>
 `;
   return node;
 }
@@ -28,6 +43,6 @@ export function displayFilms(films: Film[]): void {
   filmsListEl.innerHTML = '';
 
   films.forEach(film => {
-    filmsListEl?.appendChild(createFilmEl(film));
+    filmsListEl.appendChild(createFilmEl(film));
   });
 }
