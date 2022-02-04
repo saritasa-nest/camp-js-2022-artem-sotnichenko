7b9e5e515de3a95cc71e59e2747d4aa7e39e1e5c
Artem Sotnichenko <artem.sotnichenko@saritasa.com>
2022-02-04 14:00:06 +0700

    Add constant for batch size when fetching #JC19-255

--- vanilla/src/entities/connected-film/fetch.ts
+++ vanilla/src/entities/connected-film/fetch.ts
@@ -2,6 +2,7 @@ import { getDocs, query, where } from 'firebase/firestore';
 
 import { FirebaseWrapper } from '../../firebase/types';
 import { createCollection, mapDocumentToDto } from '../../firebase/utils';
+import { FIRESTORE_FETCH_BATCH_SIZE } from '../../utils/constants';
 
 /**
  * Fetch docs by id.
@@ -10,11 +11,17 @@ import { createCollection, mapDocumentToDto } from '../../firebase/utils';
  */
 export async function fetchDocsByIds<Doc>(collectionName: string, ids: readonly string[]): Promise<(Doc & FirebaseWrapper)[]> {
   const querySnapshotsPromises = [];
-  for (let i = 0; i < ids.length; i += 12) {
-    const documentQuery = query(createCollection<Doc>(collectionName), where('__name__', 'in', ids.slice(i, i + 10)));
+
+  for (let i = 0; i < ids.length; i += FIRESTORE_FETCH_BATCH_SIZE) {
+    const documentQuery = query(
+      createCollection<Doc>(collectionName),
+      where('__name__', 'in', ids.slice(i, i + FIRESTORE_FETCH_BATCH_SIZE)),
+    );
     const querySnapshot = getDocs<Doc>(documentQuery);
+
     querySnapshotsPromises.push(querySnapshot);
   }
+
   const querySnapshots = await Promise.all(querySnapshotsPromises);
   return querySnapshots.flatMap(querySnapshot => querySnapshot.docs.map(mapDocumentToDto));
 }
