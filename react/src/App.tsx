55074c8c442b552eeb8480d4a693d03822cc76f6
Artem Sotnichenko <artem.sotnichenko@saritasa.com>
2022-03-17 17:13:05 +0700

    Add loading indicators #JC19-296

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,13 +1,14 @@
 import { Suspense } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { LinearProgress } from '@mui/material';
 import { store } from './store';
 import { RootRouter } from './routes/RootRouter';
 
 export const App: React.VFC = () => (
   <BrowserRouter>
     <Provider store={store}>
-      <Suspense fallback={<div>Brrr... here should be your loader component</div>}>
+      <Suspense fallback={<LinearProgress />}>
         <RootRouter />
       </Suspense>
     </Provider>
--- react/src/routes/guards/Auth.tsx
+++ react/src/routes/guards/Auth.tsx
@@ -1,9 +1,10 @@
+import { LinearProgress } from '@mui/material';
 import { useEffect, VFC } from 'react';
 import { Outlet } from 'react-router-dom';
 import { AuthService } from 'src/api/services/auth.service';
 import { useAppDispatch, useAppSelector } from 'src/store';
 import { selectLoading } from 'src/store/auth/selectors';
-import { setUser } from 'src/store/auth/slice';
+import { setLoading, setUser } from 'src/store/auth/slice';
 
 /** Show loading while first auth state is loading. */
 export const AuthGuard: VFC = () => {
@@ -13,12 +14,13 @@ export const AuthGuard: VFC = () => {
   useEffect(() => {
     const unsubscribe = AuthService.subscribeToAuthChange(user => {
       dispatch(setUser(user));
+      dispatch(setLoading(false));
     });
     return unsubscribe;
   }, []);
 
   if (isLoading) {
-    return <div>loading</div>;
+    return <LinearProgress />;
   }
 
   return <Outlet />;
