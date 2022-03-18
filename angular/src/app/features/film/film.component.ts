061e431f505d8cdfb92f857eee9c58b56259c868
Artem Sotnichenko <artem.sotnichenko@saritasa.com>
2022-03-18 12:34:45 +0700

    Reduce scope of dependencies to constructor only #JC19-284

--- angular/src/app/features/film/film.component.ts
+++ angular/src/app/features/film/film.component.ts
@@ -1,9 +1,7 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable, switchMap, shareReplay, map } from 'rxjs';
-import { Character } from 'src/app/core/models/character';
 import { Film } from 'src/app/core/models/film';
-import { Planet } from 'src/app/core/models/planet';
 import { CharacterService } from 'src/app/core/services/character.service';
 import { FilmService } from 'src/app/core/services/film/film.service';
 import { PlanetService } from 'src/app/core/services/planet.service';
@@ -18,49 +16,36 @@ import { assertNotNullish } from 'src/app/core/utils/assert-not-null';
 })
 export class FilmComponent {
   /** Film. */
-  public readonly film$: Observable<Film> = this.getFilm();
+  public readonly film$: Observable<Film>;
 
   /** Character names. */
-  public readonly characterNames$: Observable<readonly string[]> = this.getCharacterNames();
+  public readonly characterNames$: Observable<readonly string[]>;
 
   /** Planet names. */
-  public readonly planetNames$: Observable<readonly string[]> = this.getPlanetNames();
+  public readonly planetNames$: Observable<readonly string[]>;
 
   public constructor(
-    private readonly route: ActivatedRoute,
-    private readonly filmService: FilmService,
-    private readonly characterService: CharacterService,
-    private readonly planetService: PlanetService,
-  ) {}
+    route: ActivatedRoute,
+    filmService: FilmService,
+    characterService: CharacterService,
+    planetService: PlanetService,
+  ) {
+    this.film$ = route.paramMap.pipe(
+      switchMap(paramMap => {
+        const id = paramMap.get('id');
+        assertNotNullish(id, 'There is no film id. This is probably route issue.');
+        return filmService.getFilm(id);
+      }),
+      shareReplay({ refCount: true, bufferSize: 1 }),
+    );
 
-  private getFilm(): Observable<Film> {
-    return this.route.paramMap
-      .pipe(
-        switchMap(paramMap => {
-          const id = paramMap.get('id');
-          assertNotNullish(id, 'There is no film id. This is probably route issue.');
-          return this.filmService.getFilm(id);
-       }),
-        shareReplay({ refCount: true, bufferSize: 1 }),
-      );
-  }
-
-  /**
-   * Get character names by ids array.
-   */
-  private getCharacterNames(): Observable<Character['name'][]> {
-    return this.film$.pipe(
-      switchMap(film => this.characterService.getCharacters(film.characterIds)
+    this.characterNames$ = this.film$.pipe(
+      switchMap(film => characterService.getCharacters(film.characterIds)
         .pipe(map(characters => characters.map(character => character.name)))),
     );
-  }
 
-  /**
-   * Get Planet names by ids array.
-   */
-  private getPlanetNames(): Observable<Planet['name'][]> {
-    return this.film$.pipe(
-      switchMap(film => this.planetService.getPlanets(film.planetIds)
+    this.planetNames$ = this.film$.pipe(
+      switchMap(film => planetService.getPlanets(film.planetIds)
         .pipe(map(planets => planets.map(planet => planet.name)))),
     );
   }
