1a236bbee9ea30a6d02c91400664a8de31a516e5
Artem Sotnichenko <artem.sotnichenko@saritasa.com>
2022-03-26 22:25:16 +0700

    Create planet, character services #JC19-296

--- react/src/api/services/firestore.service.ts
+++ react/src/api/services/firestore.service.ts
@@ -3,19 +3,22 @@ import {
   CollectionReference,
   doc,
   DocumentData,
+  documentId,
   DocumentSnapshot,
   getDoc,
   getDocs,
   query,
   QueryConstraint,
+  where,
 } from 'firebase/firestore';
-import { FirestoreDto } from '../dtos/firestore';
+import { FirestoreDto, FirestoreId } from '../dtos/firestore';
 import { firestoreMapper } from '../mappers/firestore.mapper';
 import { FirebaseService } from './firebase.service';
 
 export type FirestoreCollectionName = 'films' | 'characters' | 'planets';
 
 export namespace FirestoreService {
+  export const FIRESTORE_BATCH_MAX_SIZE = 10;
 
   /**
    * Create typed collection.
@@ -40,13 +43,38 @@ export namespace FirestoreService {
   }
 
   /**
+   * Get a stream of documents queried by array of ids.
+   * @param collectionName Collection name.
+   * @param ids Entities ids.
+   * @param constraints Query constraints.
+   */
+  export async function getManyByIds<T>(
+    collectionName: FirestoreCollectionName,
+    ids: readonly FirestoreId[],
+  ): Promise<FirestoreDto<T>[]> {
+    const entitiesBatches = [];
+
+    for (let i = 0; i < ids.length; i += FIRESTORE_BATCH_MAX_SIZE) {
+      const entitiesQuery = query<T>(
+        getCollection(collectionName),
+        where(documentId(), 'in', ids.slice(i, i + FIRESTORE_BATCH_MAX_SIZE)),
+      );
+      entitiesBatches.push(getDocs<T>(entitiesQuery));
+    }
+
+    const entities = await Promise.all(entitiesBatches);
+
+    return entities.flatMap(snapshot => snapshot.docs.map(firestoreMapper.toDto));
+  }
+
+  /**
    * Fetch document snapshot.
    * @param collectionName Collection name.
    * @param id Document id.
    */
   export function fetchSnapshot<T = DocumentData>(
     collectionName: FirestoreCollectionName,
-    id: string,
+    id: FirestoreId,
   ): Promise<DocumentSnapshot<T>> {
     return getDoc(doc(getCollection<T>(collectionName), id));
   }
