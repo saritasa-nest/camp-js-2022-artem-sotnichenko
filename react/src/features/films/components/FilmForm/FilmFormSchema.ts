75a959e77e389cd31c77fe54db1f16aa85e33310
Artem Sotnichenko <artem.sotnichenko@saritasa.com>
2022-03-29 15:52:21 +0700

    Improve film form schema #JC19-296

--- react/src/features/films/components/FilmForm/FilmFormSchema.ts
+++ react/src/features/films/components/FilmForm/FilmFormSchema.ts
@@ -2,23 +2,26 @@ import * as Yup from 'yup';
 
 export const FilmFormSchema = Yup.object().shape({
   title: Yup.string()
-    .min(3, 'Too Short!')
-    .required('Required'),
+    .min(3, 'Should be at least 3 characters long.')
+    .required('Required.'),
   openingCrawl: Yup.string()
-    .min(3, 'Too Short!')
+    .min(3, 'Should be at least 3 characters long.')
     .required('Required'),
   director: Yup.string()
-    .min(3, 'Too Short!')
+    .min(3, 'Should be at least 3 characters long.')
     .required('Required'),
-  producers: Yup.array()
-    .min(1, 'Too short!')
+  producers: Yup.array().of(
+    Yup.string()
+      .min(3, 'Should be at least 3 characters long.'),
+  )
+    .min(1, 'Should have at least 1 producer.')
     .required('Required'),
   releaseDate: Yup.string()
     .required('Required'),
   characterIds: Yup.array()
-    .min(1, 'Too short!')
+    .min(1, 'Should have at least 1 character.')
     .required('Required'),
   planetIds: Yup.array()
-    .min(1, 'Too short!')
+    .min(1, 'Should have at least 1 planet.')
     .required('Required'),
 });
