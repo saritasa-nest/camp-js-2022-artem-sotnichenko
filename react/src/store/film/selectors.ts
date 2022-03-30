dcd3d457d3769b4f0572abe395de1527f22a12c6
Artem Sotnichenko <artem.sotnichenko@saritasa.com>
2022-03-30 22:23:14 +0700

    Place active film on top of sidebar #JC19-296

--- react/src/store/film/selectors.ts
+++ react/src/store/film/selectors.ts
@@ -5,6 +5,9 @@ import { filmsAdapter } from './state';
 /** Film loading status. */
 export const selectFilmLoading = createSelector((state: RootState) => state.films.loading, loading => loading);
 
+/** Current active film id. */
+export const selectActiveFilmId = createSelector((state: RootState) => state.films.activeId, activeId => activeId);
+
 export const {
   selectAll: selectAllFilms,
   selectById: selectFilmById,
